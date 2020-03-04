958ebadcf40ab3d19ab6e7cc102456265469562b
Gabriel Kosteff Benith <60756730+GabrielBenith@users.noreply.github.com>
2020-03-03 22:06:14 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,10 @@
+#Autor: 1DSI3
+	#@Gabriel Kosteff Benith
+	#Feito em: 03/03/2020
+	#Instituição: ETEC
+
+x=0 #Declração de Contador
+valor = 2 #Declaração de variável valor
+while (x <= 21);
+	valor = valor*valor #Laço de repetição
+print (valor) #exibir a variável valor
